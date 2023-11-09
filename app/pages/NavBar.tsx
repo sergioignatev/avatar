1bf574028410b7f80b97f1f5cd23ab7d4bd44a56
SergioIgnatev <ignatevsergey17@gmail.com>
2023-11-09 19:00:02 +0300

    array

--- app/pages/NavBar.tsx
+++ app/pages/NavBar.tsx
@@ -6,8 +6,8 @@ interface Navigation{
     id:any,
     movie:string
 }
-const navbar={main:[{title:"Об Авторе",href:"/author"},{title:"Главная",href:"/"}],
-dropdown:'Фильмы',dropitems:['Добавлю Наполеон',"Че нить добавлю"]
+const navbar={main:[{id:Date.now(),title:"Об Авторе",href:"/author"},{id:Date.now(),title:"Главная",href:"/"}],
+dropdown:'Фильмы',dropitems:['Добавлю Наполеон',"Че нить добавлю",'Аватар']
 }
 export function NavBar(){
 
@@ -20,14 +20,11 @@ let randomNumber=randomArray.length
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </label>
         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <li><Link href='/author'>Об Авторе</Link></li>
-
-          <li><Link href='/'>Главная</Link></li>
+         {navbar.main.map(nav=><li key={nav.id}><Link href={nav.href}>{nav.title}</Link></li>)}
           <li>
-            <a>Фильмы</a>
+            <a>{navbar.dropdown}</a>
             <ul className="p-2">
-              <li>Наполеон</li>
-              <li><a>Че Нить добавлю</a></li>
+              {navbar.dropitems.map(item=><li key={Date.now()}><Link href='#'>{item}</Link></li>)}
             </ul>
           </li>
         </ul>
